b1271047364421035c00621411fbb6ba818ef844
qlovy <qlovy@sysnet.ch>
2023-03-19 17:50:45 +0100

    Création fonction et objet de l'échiquier

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 /*CANVAS*/
 
-const canvas = document.querySelector('#Canvas');//cela nous permet de prendre toutes les informations nécessaires.
+const canvas = document.querySelector('#canvas');//cela nous permet de prendre toutes les informations nécessaires.
 const width = canvas.widht = 800;
 const height = canvas.height = 800;
 const ctx = canvas.getContext('2d');//l'environnement du canvas, ici en deux dimensions.
@@ -16,3 +15,28 @@ const Board = function(config){
     this.color = config.color2 || 'rgb(0, 0, 0)';
 }
 
+Board.prototype.draw = function(){
+    
+    for(let i = 0; i < 10; i++){
+        
+        //choix de la couleur en fonction de i
+        console.log(i % 2);
+        if(i % 2 === 0){
+            ctx.fillStyle = this.color1;
+        }else{
+            ctx.fillStyle = this.color2;
+        }
+        
+        ctx.beginPath();
+        ctx.rect(this.x + i * this.dimension, this.y, this.dimension, this.dimension);
+        ctx.fill();
+    }
+}
+
+const TheBoard = new Board({
+    x: 0,
+    y: 0,
+    dimension: 100
+});
+
+TheBoard.draw();
